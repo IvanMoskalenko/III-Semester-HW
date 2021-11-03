0750d75aa656fe48ee5d89c8d4b7e5351bcc3c65
Ivan <ivon.klass@yandex.ru>
2021-11-03 23:48:27 +0300

    Realizations of basic methods

--- src/ThreadPool/ThreadPool/IMyTask.cs
+++ src/ThreadPool/ThreadPool/IMyTask.cs
@@ -4,7 +4,7 @@ namespace ThreadPool
 {
     public interface IMyTask<TResult>
     {
-        public bool IsCompleted { get;}
+        public bool IsCompleted { get; }
         public TResult Result { get; }
 
         public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
--- src/ThreadPool/ThreadPool/MyThreadPool.cs
+++ src/ThreadPool/ThreadPool/MyThreadPool.cs
@@ -1,42 +1,119 @@
 using System;
 using System.Collections.Concurrent;
+using System.Threading;
 
 namespace ThreadPool
 {
     public class MyThreadPool
     {
-        private ConcurrentQueue<Action> tasks;
+        private ConcurrentQueue<Action> _tasks;
+        private Thread[] _threads;
+        private CancellationTokenSource _cancellationToken;
 
         public MyThreadPool(int countOfThreads)
         {
-            throw new NotImplementedException();
+            _tasks = new ConcurrentQueue<Action>();
+            _threads = new Thread[countOfThreads];
+            _cancellationToken = new CancellationTokenSource();
+            for (var i = 0; i < countOfThreads; i++)
+            {
+                _threads[i] = CreateThread();
+                _threads[i].Start();
+            }
+            
         }
 
         private class MyTask<TResult> : IMyTask<TResult>
         {
-            public bool IsCompleted { get; }
-            public TResult Result { get; }
+            private TResult _result;
             private Func<TResult> _func;
-            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func)
+            private AggregateException _exception;
+            public bool IsCompleted { get; private set; }
+
+            public TResult Result
             {
-                throw new NotImplementedException();
+                get
+                {
+                    while (true)
+                    {
+                        if (!IsCompleted) continue;
+                        if (_exception != null)
+                        {
+                            throw _exception;
+                        }
+
+                        return _result;
+                    }
+                }
             }
+            
             public MyTask(Func<TResult> function)
             {
                 _func = function;
             }
+
+            public void Run()
+            {
+                try
+                {
+                    _result = _func();
+                }
+                catch (Exception e)
+                {
+                    _exception = new AggregateException(e);
+                }
+                IsCompleted = true;
+            }
+            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func)
+            {
+                throw new NotImplementedException();
+            }
         }
-        
+
         public IMyTask<T> Submit<T>(Func<T> task)
         {
+            lock (_cancellationToken)
+            {
+                if (_cancellationToken.Token.IsCancellationRequested)
+                {
+                    throw new ThreadInterruptedException();
+                }
+            }
             var myTask = new MyTask<T>(task);
+            _tasks.Enqueue(myTask.Run);
             return myTask;
         }
         
         
         public void Shutdown()
         {
-            throw new NotImplementedException();
+            lock (_cancellationToken)
+            {
+                _cancellationToken.Cancel();
+            }
+
+            foreach (var t in _threads)
+            {
+                t.Join();
+            }
+            
+        }
+
+        private Thread CreateThread()
+        {
+            var thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    if (_cancellationToken.Token.IsCancellationRequested && _tasks.IsEmpty)
+                    {
+                        return;
+                    }
+                    if (!_tasks.TryDequeue(out var task)) continue;
+                    task();
+                }
+            });
+            return thread;
         }
     }
 }
