f81eda50d89a63a18daeaf29fff1fb3f5e206de8
Ivan <ivon.klass@yandex.ru>
2021-11-06 15:28:10 +0300

    Tests fixes

--- src/MyFTP/MyFTP.Tests/Tests.cs
+++ src/MyFTP/MyFTP.Tests/Tests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
@@ -10,20 +9,21 @@ namespace MyFTP.Tests
 {
     public class Tests
     {
-        private const string Path = "../../../../MyFTP.Tests/TestData";
+        private readonly string _path = Path.Join("..", "..", "..", "..", "MyFTP.Tests", "TestData");
         private const string Ip = "127.0.0.1";
         private const int Port = 1337;
         
         [SetUp]
         public void Setup()
         {
+
             Server.Start(IPAddress.Parse(Ip), Port);
         }
         
         [Test]
         public async Task ListShouldReturnRightSizeAndItems()
         {
-            var actual = await Client.Run("1", Path, Ip, Port);
+            var actual = await Client.Run("1", _path, Ip, Port);
             const string expected = "5\n" 
                                     + "../../../../MyFTP.Tests/TestData/Empty2 true\n"
                                     + "../../../../MyFTP.Tests/TestData/Empty1 true\n"
@@ -35,9 +35,9 @@ namespace MyFTP.Tests
         [Test]
         public async Task GetShouldReturnRightSizeAndDownloadFile()
         {
-            var actual = await Client.Run("2", Path + "/empty1", Ip, Port, Path + "/test");
-            FileAssert.AreEqual(Path + "/empty1", Path + "/test");
-            File.Delete(Path + "/test");
+            var actual = await Client.Run("2", _path + "/empty1", Ip, Port, _path + "/test");
+            FileAssert.AreEqual(_path + "/empty1", _path + "/test");
+            File.Delete(_path + "/test");
             Assert.AreEqual("7", actual);
         }
     }
